f258e8204e3ac430b310f857a4a8892fce46e8dc
roperrott <rosarioperrotta7@gmail.com>
2023-03-26 18:44:25 -0300

    movies and songs crud with redux toolkit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { songsReducer, addSong, removeSong } from './slices/songsSlice';
+import { moviesReducer, addMovie, removeMovie } from './slices/moviesSlice';
+import { reset } from './actions';
+
+const store = configureStore({
+    reducer: {
+        songs: songsReducer,
+        movies: moviesReducer,
+    }
+});
+
+
+export {  };
+export { store, addSong, removeSong, addMovie, removeMovie, reset };
+
+
